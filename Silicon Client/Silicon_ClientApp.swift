4e26d11341c50e4b656efe5980c6e43a2fe18894
Jasper <yuansun0504@gmail.com>
2026-08-20 17:19:06 -0700

    Initial Commit

---
+++ Silicon Client/Silicon_ClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  Silicon_ClientApp.swift
+//  Silicon Client
+//
+//  Created by Yuan Sun on 8/20/26.
+//
+
+import SwiftUI
+
+@main
+struct Silicon_ClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
